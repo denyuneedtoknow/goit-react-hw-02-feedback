492d1a8636eff0f53b90b74be2b486f76ea81ce1
Denys Hruzdiev <denyuneedtoknow@gmail.com>
2021-10-05 17:02:09 +0300

    Counter added

---
+++ src/Counter/Counter.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+
+class Counter extends React.Component {
+    state = {
+        value: this.props.initialValue,
+    }
+
+    handleIncrement = () => {
+        this.setState(prevState => ({
+            value: prevState.value + 1,
+        }))
+    }
+
+    handleDecrement = () => {
+        this.setState(prevState => ({
+            value: prevState.value - 1,
+        }))
+    }
+
+    render() {
+        return (
+            <div className="Counter">
+                <span>{this.state.value}</span>
+                <div>
+                    <button type="button" onClick={this.handleIncrement}>+</button>
+                    <button type="button" onClick={this.handleDecrement}>-</button>
+                </div>
+            </div>
+        )
+    }
+}
+export default Counter
