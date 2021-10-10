59785342f5a4cac6e20678bf0ce1c26a07c5d0e3
Denys Hruzdiev <denyuneedtoknow@gmail.com>
2021-10-10 14:02:59 +0300

    Feedback started

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
-import Counter from "./Counter/Counter"
+import "./App.css";
+import Counter from "./Counter/Counter";
+import Feedback from "./Feedback/Feedback";
 
 function App() {
   return (
     <div className="App">
       <Counter initialValue={0} />
+      <Feedback initialValue={{ good: 0, neutral: 0, bad: 0 }} />
     </div>
   );
 }
 
 export default App;
-
--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -1,36 +1,39 @@
 import React from "react";
 
-
 class Counter extends React.Component {
-    static defaultProps = {
-        initialValue: 0,
-    };
-    state = {
-        value: this.props.initialValue,
-    }
+  static defaultProps = {
+    initialValue: 0,
+  };
+  state = {
+    value: this.props.initialValue,
+  };
 
-    handleIncrement = () => {
-        this.setState(prevState => ({
-            value: prevState.value + 1,
-        }))
-    }
+  handleIncrement = () => {
+    this.setState((prevState) => ({
+      value: prevState.value + 1,
+    }));
+  };
 
-    handleDecrement = () => {
-        this.setState(prevState => ({
-            value: prevState.value - 1,
-        }))
-    }
+  handleDecrement = () => {
+    this.setState((prevState) => ({
+      value: prevState.value - 1,
+    }));
+  };
 
-    render() {
-        return (
-            <div className="Counter">
-                <span>{this.state.value}</span>
-                <div>
-                    <button type="button" onClick={this.handleIncrement}>+</button>
-                    <button type="button" onClick={this.handleDecrement}>-</button>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="Counter">
+        <span>{this.state.value}</span>
+        <div>
+          <button type="button" onClick={this.handleIncrement}>
+            +
+          </button>
+          <button type="button" onClick={this.handleDecrement}>
+            -
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
-export default Counter
+export default Counter;
